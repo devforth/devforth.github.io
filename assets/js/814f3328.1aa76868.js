9e92e29d20553fa2e0cd7e59eb404d85a99d340d
Ivan Borshchov <ivan@devforth.io>
2024-10-01 22:20:32 +0300

    Deploy website - based on 678d8a1e03f216e7defe5b39703ba9979f689b48

--- assets/js/814f3328.1aa76868.js
+++
@@ -1,1 +0,0 @@
-"use strict";(self.webpackChunkadminforth=self.webpackChunkadminforth||[]).push([[7472],{5513:t=>{t.exports=JSON.parse('{"title":"Recent posts","items":[{"title":"Chat-GPT plugin to co-write texts and strings","permalink":"/blog/chatgpt-plugin","unlisted":false}]}')}}]);
